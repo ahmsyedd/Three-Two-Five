da2843658f6ff2658cf1c73ac16fccd03356e04f
ahmsyedd <98047590+ahmsyedd@users.noreply.github.com>
2022-09-16 21:46:43 -0400

    Update Game.java

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -1,18 +1,7 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
 
-/**
- * The class that models your game. You should create a more specific child of
- * this class and instantiate the methods given.
- *
- * @author dancye, 2018
- */
 public class Game {
 
     private ArrayList<Player> players;// the players of the game
