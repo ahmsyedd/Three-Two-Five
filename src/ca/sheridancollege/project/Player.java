c683b61fdf742351ee60a2b32744bd243cfb04a7
ahmsyedd <98047590+ahmsyedd@users.noreply.github.com>
2022-09-16 21:47:56 -0400

    Update Player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,19 +1,8 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-/**
- * A class that models each Player in the game. Players have an identifier,
- * which should be unique.
- *
- * @author dancye, 2018
- */
 public class Player {
 
     // variables
