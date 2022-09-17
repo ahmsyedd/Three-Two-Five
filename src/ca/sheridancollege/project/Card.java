5b99b5dbe5c97aa170678066922254137301835d
ahmsyedd <98047590+ahmsyedd@users.noreply.github.com>
2022-09-16 21:47:08 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -1,15 +1,5 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
 package ca.sheridancollege.project;
 
-/**
- * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
- */
 public class Card 
 {
     //default modifier for child classes
