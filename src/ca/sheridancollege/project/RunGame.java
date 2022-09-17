cb3a4ecd67b026684ecfeb2e80d1559f46d7b0e2
ahmsyedd <98047590+ahmsyedd@users.noreply.github.com>
2022-09-16 21:48:21 -0400

    Update RunGame.java

--- src/ca/sheridancollege/project/RunGame.java
+++ src/ca/sheridancollege/project/RunGame.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author Devansh
- */
 public class RunGame {
 
     public static void main(String[] args) throws InterruptedException {
